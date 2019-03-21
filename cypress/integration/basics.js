4ab9244e38fa5c8fc498be1a5c5bc17d4db5c36a
Strajk <strajk@me.com>
2019-03-21 20:30:25 +0100

    Basic Cypress, disable Disqus

---
+++ cypress/integration/basics.js
@@ -0,0 +1,16 @@
+context("QA Academy", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
+  it("open all pages and check if there are no errors", () => {
+    const selector = ".sidebar-nav > ul > li > a"
+
+    cy.get(selector)
+      .each(($el, index) => {
+        cy.get(selector + `:eq(${index})`).click()
+        cy.wait(1000)
+        cy.get(".content h1").should("be.visible")
+      })
+  })
+})
