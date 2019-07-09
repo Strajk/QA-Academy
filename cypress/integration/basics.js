394e4104ea22e3c2e3ce89d3c88f0ebfc1d030bb
Strajk <strajk@me.com>
2019-07-09 11:38:42 +0200

    Basic screenshot testing

--- cypress/integration/basics.js
+++ cypress/integration/basics.js
@@ -11,6 +11,9 @@ context("QA Academy", () => {
         cy.get(selector + `:eq(${index})`).click()
         cy.wait(1000)
         cy.get(".content h1").should("be.visible")
+        cy.screenshot($el.text(), {
+          capture: "viewport" // Cypress is really struggling to capture full page - it's total mess
+        })
       })
   })
 })
